582bcaec101f7be1db0763e4dc39e989b0efaa40
julien <julien.richarte@gmail.com>
2014-08-01 22:30:21 +0200

    [Upg] syntara versions

--- ansible/scripts/fill_syntara_composer.php
+++ ansible/scripts/fill_syntara_composer.php
@@ -4,6 +4,6 @@ $rootPath = "/vagrant/";
 
 $composerJson = file_get_contents($rootPath . 'composer.json');
 $composerArray = json_decode($composerJson, true);
-$composerArray['require']['mrjuliuss/syntara'] = "1.2.*";
+$composerArray['require']['mrjuliuss/syntara'] = $argv[1];
 
 file_put_contents($rootPath . 'composer.json', json_encode($composerArray));
